f62de247bdcbed8be7971fb4a2d0ecddcfa361a1
Caroline Voagen Nelson <nelsc182@newschool.edu>
2014-10-24 13:50:24 -0400

    This is a test

---
+++ Assets/scripts/instantiate.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class instantiate : MonoBehaviour {
+
+
+	public GameObject stoop;
+	public float stxPos;
+	public float styPos;
+	public float stzPos;
+	public GameObject chrysler;
+	public float boxPos;
+	public float boyPos;
+	public float bozPos;
+	public GameObject fence;
+	public float fxPos;
+	public float fyPos;
+	public float fzPos;
+	public GameObject bridge;
+	public float bxPos;
+	public float byPos;
+	public float bzPos;
+
+
+
+	//public GameObject gameObject1 = null;
+	// Use this for initialization
+	void Start () {
+
+		for (int i=1; i <=10; i++) {
+			Vector3 position = new Vector3 (stxPos, styPos, i*stzPos++);
+			//I can rename GameObject that is green
+			
+			// call the game object- action folder, load folder place postion, rotation, and as is to cast
+			stoop = Instantiate (Resources.Load ("Prefab/stoop"), position, Quaternion.identity) as GameObject;
+
+		
+			
+		}
+		
+		for (int i=1; i <=30; i++) {
+			Vector3 position = new Vector3 (boxPos, boyPos++, bozPos);
+			//I can rename GameObject that is green
+
+			boxPos++;
+
+			// call the game object- action folder, load folder place postion, rotation, and as is to cast
+			chrysler= Instantiate (Resources.Load ("Prefab/chrysler"), position, Quaternion.identity) as GameObject;
+			
+		}
+		for (int i=1; i <=20; i++) {
+			Vector3 position = new Vector3 (fxPos, fyPos++, fzPos);
+			//I can rename GameObject that is green
+			
+			boxPos++;
+			
+			// call the game object- action folder, load folder place postion, rotation, and as is to cast
+			fence= Instantiate (Resources.Load ("Prefab/fence"), position, Quaternion.identity) as GameObject;
+			
+		}
+		for (int i=1; i <=40; i++) {
+			Vector3 position = new Vector3 (i*bxPos++, i*byPos, bzPos);
+			//I can rename GameObject that is green
+			
+			boxPos++;
+			
+			// call the game object- action folder, load folder place postion, rotation, and as is to cast
+			bridge= Instantiate (Resources.Load ("Prefab/bridge"), position, Quaternion.identity) as GameObject;
+			
+		}
+	
+
+
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			
+			Application.LoadLevel(0); //or whatever number your scene is
+			
+		}
+	
+	}
+}
---
+++ Assets/scripts/nycGenerator.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nycGenerator : MonoBehaviour {
+
+	public Transform bridge;
+	public float brxPos;
+	public float bryPos;
+	public float brzPos;
+	public Transform building;
+	public float blxPos;
+	public float blyPos;
+	public float blzPos;
+	public Transform taxi;
+	public float txPos;
+	public float tyPos;
+	public float tzPos;
+	public Transform pizza;
+	public float pxPos;
+	public float pyPos;
+	public float pzPos;
+	public Transform ninjaTurtle;
+	public float nxPos;
+	public float nyPos;
+	public float nzPos;
+	public Transform money;
+	public float mxPos;
+	public float myPos;
+	public float mzPos;
+	public TextMesh nyc;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ( NycGen() );
+	}
+	
+	IEnumerator NycGen() {
+		
+		int i = 0;
+		while ( i<1 )  {
+			
+			yield return new WaitForSeconds ( 1f );
+			
+			Instantiate(bridge, new Vector3(brxPos, bryPos, brzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			Instantiate(building, new Vector3(blxPos, blyPos, blzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			Instantiate(taxi, new Vector3(txPos, tyPos, tzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			Instantiate(pizza, new Vector3(pxPos, pyPos, pzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			Instantiate(ninjaTurtle, new Vector3(nxPos, nyPos, nzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			Instantiate(money, new Vector3(mxPos, myPos, mzPos), Quaternion.identity); 
+			yield return new WaitForSeconds ( 0.2f );
+			
+			i++;
+			
+		}
+		
+	}
+	
+	void Update() {
+
+		nyc.transform.Translate(Vector3.right * .5f * Time.deltaTime);
+		
+		if(Input.GetKeyDown(KeyCode.R)){
+			
+			Application.LoadLevel(0); //or whatever number your scene is
+			
+		}
+		
+	}
+
+}
